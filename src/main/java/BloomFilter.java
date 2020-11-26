05a93368c6a796f1b2048f2090b840179a0fabe4
Melvin Johner <melvin.johner@students.fhnw.ch>
2020-11-26 14:55:35 +0100

    calculation of filter params

--- src/main/java/BloomFilter.java
+++ src/main/java/BloomFilter.java
@@ -1,16 +1,20 @@
+import java.util.Arrays;
+
 public class BloomFilter {
 
     private int elements_n;
     private double errorProb_p;
     private int filterSize_m;
     private int hashFunctions_k;
+    private boolean[] bitVec;
 
-    public BloomFilter(int n, double p, int m, int k){
+    public BloomFilter(int n, double p){
         this.elements_n = n;
         this.errorProb_p = p;
-        this.filterSize_m = m;
-        this.hashFunctions_k = k;
-
+        // http://matthias.vallentin.net/blog/2011/06/a-garden-variety-of-bloom-filters/
+        filterSize_m =  (int) (-1 * Math.round((n * Math.log(p)) / (Math.pow(Math.log(2), 2))));
+        hashFunctions_k = (int) Math.round(filterSize_m / n * Math.log(2));
+        bitVec = new boolean[filterSize_m];
     }
 
     public void add(String s){
@@ -26,5 +30,13 @@ public class BloomFilter {
     public double contains(String s){
 
         return 0.0;
+    }
+
+    public void printSpecs(){
+        System.out.println("Expected number of elements: " + elements_n);
+        System.out.println("Declared error tolerance: " + errorProb_p);
+        System.out.println("Number of bits in filter vector: " + filterSize_m);
+        System.out.println("Number of hash functions: " + hashFunctions_k);
+        System.out.println("Filter vector: " + Arrays.toString(bitVec));
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args){
+        BloomFilter b = new BloomFilter(20, 0.1);
+        b.printSpecs();
 
     }
 }
